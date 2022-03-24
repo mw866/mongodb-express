5de7b31d7c5accf1aca23a0bd981b05f7cd89f15
Chris Wang <git@chriswang.tech>
2022-03-24 14:19:20 +0800

    added some API endpoint

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 A demo Expresss app connected to MongoDB with exposed connection string.
 
 ```
-docker build . -t mw866/mongodb-express:latest
-docker push mw866/mongodb-express:latest
+docker build . -t mw866/mongodb-express:latest -t mw866/mongodb-express:1.1.0 
+docker push mw866/mongodb-express -a
 ```
--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const express = require('express');
 const app = express();
 // Intentionally INSECURE!
 const MONGODB_CONNECTION_STRING = 'mongodb://wiz:correcthorsebatterystaple@i-057c1117ffe80aff3.ap-southeast-1.compute.internal:27017';
+
 const MONGODB_DATABASE = 'testdb'
 const PORT = process.env.PORT || 80
 let db;
 
 app.listen(PORT, function () {
-    console.log('Listening on port ${PORT}');
+    console.log('Listening on port:' , PORT);
 })
 
-app.route('/').get((req, res) => {
+app.get('/', (req, res) => {
+    res.send('<a href="/user">/user</a><br><a href="/user/1">/user/{uid}</a>');
+})
+
+app.get('/user', (req, res) => {
     db.collection('test').find({}).toArray((err, artifact) => {
-        if (err) throw err;
-        res.send(artifact);
+        console.error(err);
+        res.json(artifact);
     })
 })
 
+app.get('/user/:uid',(req, res) => {
+    const uid = req.params.uid;
+    console.log(req.params);
+    db.collection('test').find({uid: uid}).toArray((err, artifact) => {
+        console.error(err);
+        res.json(artifact);
+    })
+})
 mongo.connect(MONGODB_CONNECTION_STRING, (err, client) => {
     if (err) {
         console.error(err);
